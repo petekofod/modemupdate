6cc158a84808e0ac12b3b4198bdabd898f64d8e8
gin <gennady.alimov@gmail.com>
2021-02-17 21:48:30 +0300

    added inventory script

---
+++ inventory.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+list=$1
+SSH_KEY=/home/railwaynet/.ssh/id_rsa_rwn
+
+get_ssh_connect_status() {
+  </dev/null ssh -q -i ${SSH_KEY}  ${MPLS_INTERFACE_SSH} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p ${MODEM_SSH_PORT}  -o ConnectTimeout=3 -o ConnectionAttempts=1 ${MODEM_SSH_USER}@${MODEM_SSH_ADDR} "echo" 2>> log.txt
+  return $?
+}
+
+t_now=`date +%Y%m%d.%H:%M:%S`
+report_file_name=report_${t_now}.txt
+echo "$report_file_name"
+
+while read line; do
+
+modem=( $line )
+
+MODEM_SSH_ADDR=${modem[0]%\"}
+MODEM_SSH_ADDR=${MODEM_SSH_ADDR#\"}
+
+MODEM_SSH_PORT=${modem[1]%\"}
+MODEM_SSH_PORT=${MODEM_SSH_PORT#\"}
+
+MODEM_SSH_USER=${modem[2]%\"}
+MODEM_SSH_USER=${MODEM_SSH_USER#\"}
+
+MODEM_SSH_PASS=${modem[3]%\"}
+MODEM_SSH_PASS=${MODEM_SSH_PASS#\"}
+
+MODEM_LOGIN=${modem[4]%\"}
+MODEM_LOGIN=${MODEM_LOGIN#\"}
+
+MODEM_PASSWORD=${modem[5]%\"}
+MODEM_PASSWORD=${MODEM_PASSWORD#\"}
+
+MPLS_INTERFACE_SSH=""
+MPLS_INTERFACE_SCP=""
+MPLS_INTERFACE="Verizon"
+
+if [[ "${MODEM_SSH_ADDR}" == *".149."* ]]; then
+    MPLS_INTERFACE_SSH=" -b 10.102.10.37 "
+    MPLS_INTERFACE_SCP=" -o BindAddress=10.102.10.37 "
+    MPLS_INTERFACE="ATT"
+fi
+
+
+echo
+echo "***********************************************************************************************************"
+echo
+echo MODEM_SSH_ADDR=${MODEM_SSH_ADDR}
+echo MODEM_SSH_PORT=${MODEM_SSH_PORT}
+echo MODEM_SSH_USER=${MODEM_SSH_USER}
+echo MODEM_SSH_PASS=${MODEM_SSH_PASS}
+echo
+echo MODEM_LOGIN=${MODEM_LOGIN}
+echo MODEM_PASSWORD=${MODEM_PASSWORD}
+echo
+echo SSH_INTERFACE=${MPLS_INTERFACE_SSH}
+echo SCP_INTERFACE=${MPLS_INTERFACE_SCP}
+
+#Try to connect to internal host
+get_ssh_connect_status
+if [[ $? -ne 0 ]]
+then
+        msg="${MODEM_SSH_ADDR} : SSH connection broken."
+        echo ${msg}
+        echo ${msg} >> ${report_file_name}
+	      exit 111
+fi
+
+echo >> "${report_file_name}"
+echo >> "${report_file_name}"
+echo >> "${report_file_name}"
+
+</dev/null  scp -q -o StrictHostKeyChecking=no -i ${SSH_KEY} ${MPLS_INTERFACE_SCP} -o UserKnownHostsFile=/dev/null -P ${MODEM_SSH_PORT} modem_info.sh ${MODEM_SSH_USER}@${MODEM_SSH_ADDR}:/tmp/
+
+#for MODEM_IP in "192.168.13.31" "192.168.13.31"
+for MODEM_IP in "10.255.255.248" "10.255.255.249"
+do
+    echo
+    MODEM_URL="http://"${MODEM_IP}":9191"
+    echo "${MODEM_SSH_ADDR}/${MODEM_IP}" >> "${report_file_name}"
+    </dev/null ssh -q -o StrictHostKeyChecking=no -i ${SSH_KEY} ${MPLS_INTERFACE_SSH} -o UserKnownHostsFile=/dev/null -p ${MODEM_SSH_PORT} ${MODEM_SSH_USER}@${MODEM_SSH_ADDR} \
+    "/tmp/modem_info.sh" ${MODEM_URL} ${MODEM_LOGIN} ${MODEM_PASSWORD} >> "${report_file_name}"
+done
+
+done <"${list}"
---
+++ modem_info.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+MODEM_URL="$1"
+MODEM_LOGIN="$2"
+MODEM_PASSWORD="$3"
+
+#echo ${MODEM_URL}
+#echo ${MODEM_LOGIN}
+#echo ${MODEM_PASSWORD}
+
+lbl[4]="ALEOS Software Version"
+lbl[7]="Device Model"
+lbl[8]="Radio Firmware Version"
+lbl[9]="Radio Module Type"
+lbl[11220]="Radio Module Identifier"
+lbl[11227]="Radio Module Firmware short version"
+
+get_status() {
+  local status=$(curl -b coo -k \
+                -F "var=1" \
+                ${MODEM_URL}/admin/fw_status.cgi)
+
+  echo ${status}
+}
+echo
+
+curl -s -c coo -k --data "<request xmlns=\"urn:ace-c manager\"><connect><login>${MODEM_LOGIN}</login><password>${MODEM_PASSWORD}</password></connect></request>" ${MODEM_URL}/xml/Connect.xml > login_out.txt
+
+for i in 7 4 11220 11227 9 8
+do
+	val=$(curl -s -k -b coo -X POST -H "Content-Type: text/plain" -q --data ${i}\
+   	${MODEM_URL}/cgi-bin/Embedded_Ace_Get_Task.cgi)
+	val=${val#"${i}="}
+	val=${val%"!"}
+echo ${lbl[i]}: $val
+done
+echo
+
+fw_lst=$(curl -s -k  -b coo  ${MODEM_URL}/admin/rm_switching_list.cgi)
+
+printf  "Network Operator\tFirmware Version\t\tUp to date\n"
+
+delimiter="NLINE"
+fw_no=()
+while [[ $fw_lst ]]; do
+  fw_no+=( "${fw_lst%%"$delimiter"*}" )
+  fw_lst=${fw_lst#*"$delimiter"}
+done
+
+for fwlno in ${fw_no[@]}
+do
+delimiter="HSEP"
+fwlno=$fwlno$delimiter
+fwano=()
+while [[ $fwlno ]]; do
+  fwano+=( "${fwlno%%"$delimiter"*}" )
+  fwlno=${fwlno#*"$delimiter"}
+done
+
+s="${fwano[0]}\t\t\t${fwano[1]}\t${fwano[2]}\n"
+printf $s
+done
+
+echo
