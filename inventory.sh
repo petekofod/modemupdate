ca69e06d0cc5e7c595d3a6a2f7a9c5be5ef0f85b
gin <gennady.alimov@gmail.com>
2021-02-19 15:08:31 +0300

    LOCONAME and alternate WAN IP added

--- inventory.sh
+++ inventory.sh
@@ -4,7 +4,7 @@ list=$1
 SSH_KEY=/home/railwaynet/.ssh/id_rsa_rwn
 
 get_ssh_connect_status() {
-  </dev/null ssh -q -i ${SSH_KEY}  ${MPLS_INTERFACE_SSH} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p ${MODEM_SSH_PORT}  -o ConnectTimeout=3 -o ConnectionAttempts=1 ${MODEM_SSH_USER}@${MODEM_SSH_ADDR} "echo" 2>> log.txt
+  </dev/null ssh -q -i ${SSH_KEY}  ${MPLS_INTERFACE_SSH} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p ${MODEM_SSH_PORT}  -o ConnectTimeout=3 -o ConnectionAttempts=1 ${MODEM_SSH_USER}@${WAN_IP} "echo" 2>> log.txt
   return $?
 }
 
@@ -16,39 +16,36 @@ while read line; do
 
 modem=( $line )
 
-  MODEM_SSH_ADDR=${modem[0]%\"}
+  MODEM_LOCONAME=${modem[0]%\"}
+  MODEM_LOCONAME=${MODEM_LOCONAME#\"}
+
+  MODEM_SSH_ADDR=${modem[1]%\"}
   MODEM_SSH_ADDR=${MODEM_SSH_ADDR#\"}
 
-  MODEM_SSH_PORT=${modem[1]%\"}
+  MODEM_SSH_ADDR_ALT=${modem[2]%\"}
+  MODEM_SSH_ADDR_ALT=${MODEM_SSH_ADDR_ALT#\"}
+
+  MODEM_SSH_PORT=${modem[3]%\"}
   MODEM_SSH_PORT=${MODEM_SSH_PORT#\"}
 
-  MODEM_SSH_USER=${modem[2]%\"}
+  MODEM_SSH_USER=${modem[4]%\"}
   MODEM_SSH_USER=${MODEM_SSH_USER#\"}
 
-  MODEM_SSH_PASS=${modem[3]%\"}
+  MODEM_SSH_PASS=${modem[5]%\"}
   MODEM_SSH_PASS=${MODEM_SSH_PASS#\"}
 
-  MODEM_LOGIN=${modem[4]%\"}
+  MODEM_LOGIN=${modem[6]%\"}
   MODEM_LOGIN=${MODEM_LOGIN#\"}
 
-  MODEM_PASSWORD=${modem[5]%\"}
+  MODEM_PASSWORD=${modem[7]%\"}
   MODEM_PASSWORD=${MODEM_PASSWORD#\"}
-
-  MPLS_INTERFACE_SSH=""
-  MPLS_INTERFACE_SCP=""
-  MPLS_INTERFACE="Verizon"
-
-  if [[ "${MODEM_SSH_ADDR}" == *".149."* ]]; then
-      MPLS_INTERFACE_SSH=" -b 10.102.10.37 "
-      MPLS_INTERFACE_SCP=" -o BindAddress=10.102.10.37 "
-      MPLS_INTERFACE="ATT"
-  fi
-
 
   echo
   echo "***********************************************************************************************************"
   echo
+  echo MODEM_LOCONAME=${MODEM_LOCONAME}
   echo MODEM_SSH_ADDR=${MODEM_SSH_ADDR}
+  echo MODEM_SSH_ADDR_ALT=${MODEM_SSH_ADDR_ALT}
   echo MODEM_SSH_PORT=${MODEM_SSH_PORT}
   echo MODEM_SSH_USER=${MODEM_SSH_USER}
   echo MODEM_SSH_PASS=${MODEM_SSH_PASS}
@@ -59,30 +56,45 @@ modem=( $line )
   echo SSH_INTERFACE=${MPLS_INTERFACE_SSH}
   echo SCP_INTERFACE=${MPLS_INTERFACE_SCP}
 
-  #Try to connect to internal host
-  get_ssh_connect_status
-  if [[ $? -ne 0 ]]
-  then
-          msg="${MODEM_SSH_ADDR} : SSH connection broken."
-          echo ${msg}
-          echo ${msg} >> ${report_file_name}
-          continue
-  fi
 
-  echo >> "${report_file_name}"
-  echo >> "${report_file_name}"
-  echo >> "${report_file_name}"
-
-  </dev/null  scp -q -o StrictHostKeyChecking=no -i ${SSH_KEY} ${MPLS_INTERFACE_SCP} -o UserKnownHostsFile=/dev/null -P ${MODEM_SSH_PORT} modem_info.sh ${MODEM_SSH_USER}@${MODEM_SSH_ADDR}:/tmp/
-
-  #for MODEM_IP in "192.168.13.31" "192.168.13.31"
-  for MODEM_IP in "10.255.255.248" "10.255.255.249"
+  for WAN_IP in ${MODEM_SSH_ADDR} ${MODEM_SSH_ADDR_ALT}
   do
-      echo
-      MODEM_URL="http://"${MODEM_IP}":9191"
-      echo "${MODEM_SSH_ADDR}/${MODEM_IP}" >> "${report_file_name}"
-      </dev/null ssh -q -o StrictHostKeyChecking=no -i ${SSH_KEY} ${MPLS_INTERFACE_SSH} -o UserKnownHostsFile=/dev/null -p ${MODEM_SSH_PORT} ${MODEM_SSH_USER}@${MODEM_SSH_ADDR} \
-      "/tmp/modem_info.sh" ${MODEM_URL} ${MODEM_LOGIN} ${MODEM_PASSWORD} >> "${report_file_name}"
+    get_ssh_connect_status
+    if [[ $? -ne 0 ]]
+    then
+      #Try to connect to internal host
+      msg="LOCO ID: ${MODEM_LOCONAME} : WAN_IP: ${WAN_IP} : SSH connection broken."
+      echo ${msg}
+      echo ${msg} >> ${report_file_name}
+      continue
+    fi
+
+    echo >> "${report_file_name}"
+    echo >> "${report_file_name}"
+    echo >> "${report_file_name}"
+
+
+    MPLS_INTERFACE_SSH=""
+    MPLS_INTERFACE_SCP=""
+    MPLS_INTERFACE="Verizon"
+
+    if [[ "${WAN_IP}" == *".149."* ]]; then
+        MPLS_INTERFACE_SSH=" -b 10.102.10.37 "
+        MPLS_INTERFACE_SCP=" -o BindAddress=10.102.10.37 "
+        MPLS_INTERFACE="ATT"
+    fi
+
+    </dev/null  scp -q -o StrictHostKeyChecking=no -i ${SSH_KEY} ${MPLS_INTERFACE_SCP} -o UserKnownHostsFile=/dev/null -P ${MODEM_SSH_PORT} modem_info.sh ${MODEM_SSH_USER}@${WAN_IP}:/tmp/
+
+    #for MODEM_IP in "192.168.13.31" "192.168.13.31"
+    for MODEM_IP in "10.255.255.248" "10.255.255.249"
+    do
+        echo
+        MODEM_URL="http://"${MODEM_IP}":9191"
+        echo "LOCO ID: ${MODEM_LOCONAME} : ${MODEM_IP}" >> "${report_file_name}"
+        </dev/null ssh -q -o StrictHostKeyChecking=no -i ${SSH_KEY} ${MPLS_INTERFACE_SSH} -o UserKnownHostsFile=/dev/null -p ${MODEM_SSH_PORT} ${MODEM_SSH_USER}@${WAN_IP} \
+        "/tmp/modem_info.sh" ${MODEM_URL} ${MODEM_LOGIN} ${MODEM_PASSWORD} >> "${report_file_name}"
+    done
   done
 
 done <"${list}"
